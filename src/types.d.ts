889246c26ea323d6890a2053d8ab429009c2f726
Tomáš Chochola <chocholatom1997@gmail.com>
2024-09-10 00:53:03 +0200

    Update asset types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -22,3 +22,73 @@ declare module '*.svg' {
   const value: string;
   export default value;
 }
+
+declare module '*.webp' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.bmp' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.ico' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.heif' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.heic' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.tif' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.tiff' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.avif' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.mp4' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.webm' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.ogg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.mp3' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.wav' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.flac' {
+  const value: string;
+  export default value;
+}
